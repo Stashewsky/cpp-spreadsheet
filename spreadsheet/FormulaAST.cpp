c2ec0cd629afcd137a33fce7264238e1d5168245
Stanislav Polischuk <polischuk.stanislav@gmail.com>
2024-08-31 10:22:15 +0300

    Final diploma project: Spreadsheet

--- spreadsheet/FormulaAST.cpp
+++ spreadsheet/FormulaAST.cpp
@@ -72,7 +72,7 @@ public:
     virtual ~Expr() = default;
     virtual void Print(std::ostream& out) const = 0;
     virtual void DoPrintFormula(std::ostream& out, ExprPrecedence precedence) const = 0;
-    virtual double Evaluate(/*добавьте сюда нужные аргументы*/ args) const = 0;
+    virtual double Evaluate(std::function<double(Position)>& args) const = 0;
 
     // higher is tighter
     virtual ExprPrecedence GetPrecedence() const = 0;
@@ -142,8 +142,28 @@ public:
         }
     }
 
-    double Evaluate(/*добавьте нужные аргументы*/) const override {
-			// Скопируйте ваше решение из предыдущих уроков.
+    double Evaluate(std::function<double(Position)>& args) const override {
+        switch(type_) {
+            case Add:
+                return lhs_->Evaluate(args) + rhs_->Evaluate(args);
+                break;
+            case Subtract:
+                return lhs_->Evaluate(args) - rhs_->Evaluate(args);
+                break;
+            case Divide:
+                if(rhs_->Evaluate(args) != 0){
+                    return lhs_->Evaluate(args) / rhs_->Evaluate(args);
+                }else{
+                    throw FormulaError(FormulaError::Category::Arithmetic);
+                }
+                break;
+            case Multiply:
+                return lhs_->Evaluate(args) * rhs_->Evaluate(args);
+                break;
+            default:
+                throw FormulaError(FormulaError::Category::Value);
+                break;
+        }
     }
 
 private:
@@ -180,8 +200,17 @@ public:
         return EP_UNARY;
     }
 
-    double Evaluate(/*добавьте нужные аргументы*/ args) const override {
-        // Скопируйте ваше решение из предыдущих уроков.
+    double Evaluate(std::function<double(Position)>& args) const override {
+        switch (type_) {
+            case UnaryPlus:
+                return operand_->Evaluate(args);
+                break;
+            case UnaryMinus:
+                return -operand_->Evaluate(args);
+                break;
+            default:
+                throw FormulaError(FormulaError::Category::Value);
+        }
     }
 
 private:
@@ -211,8 +240,11 @@ public:
         return EP_ATOM;
     }
 
-    double Evaluate(/*добавьте нужные аргументы*/ args) const override {
-        // реализуйте метод.
+    double Evaluate(std::function<double(Position)>& args) const override {
+        if(!cell_->IsValid()){
+            throw FormulaError(FormulaError::Category::Ref);
+        }
+        return args(*cell_);
     }
 
 private:
@@ -237,7 +269,7 @@ public:
         return EP_ATOM;
     }
 
-    double Evaluate(/*добавьте нужные аргументы*/ args) const override {
+    double Evaluate(std::function<double(Position)>& args) const override {
         return value_;
     }
 
@@ -391,8 +423,8 @@ void FormulaAST::PrintFormula(std::ostream& out) const {
     root_expr_->PrintFormula(out, ASTImpl::EP_ATOM);
 }
 
-double FormulaAST::Execute(/*добавьте нужные аргументы*/ args) const {
-    return root_expr_->Evaluate(/*добавьте нужные аргументы*/ args);
+double FormulaAST::Execute(std::function<double(Position)>& args) const {
+    return root_expr_->Evaluate(args);
 }
 
 FormulaAST::FormulaAST(std::unique_ptr<ASTImpl::Expr> root_expr, std::forward_list<Position> cells)
--- spreadsheet/sheet.h
+++ spreadsheet/sheet.h
@@ -1,1 +1,41 @@
-/* разместите здесь свой код */
+#pragma once
+
+#include "cell.h"
+#include "common.h"
+#include <map>
+#include <set>
+#include <functional>
+
+class Sheet : public SheetInterface {
+public:
+    ~Sheet();
+
+    void SetCell(Position pos, std::string text) override;
+
+    const CellInterface* GetCell(Position pos) const override;
+    CellInterface* GetCell(Position pos) override;
+
+    void ClearCell(Position pos) override;
+
+    Size GetPrintableSize() const override;
+
+    void PrintValues(std::ostream& output) const override;
+    void PrintTexts(std::ostream& output) const override;
+
+    // Можете дополнить ваш класс нужными полями и методами
+    void InvalidateCell(Position pos);
+    void AddDependCell(const Position ref_cell, const Position depend_cell);
+    const std::set<Position> GetDependentCells(const Position pos);
+    void RemoveDependency(const Position pos);
+
+private:
+    // Можете дополнить ваш класс нужными полями и методами
+    struct PositionHasher {
+        size_t operator()(const Position& pos) const {
+            return std::hash<int>()(pos.row) ^ (std::hash<int>()(pos.col) << 1);
+        }
+    };
+
+    std::unordered_map<Position, std::unique_ptr<Cell>, PositionHasher> sheet_;
+    std::map<Position, std::set<Position>> cells_dependencies_;
+};
